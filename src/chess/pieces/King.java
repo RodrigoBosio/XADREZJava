65dc52aa222c4e5f869e16f53e8a8c6e182c6c8c
Rodrigo Bosio <rodrigo.bosio@fatec.sp.gov.br>
2019-07-26 17:37:05 -0300

    Placing pieces on the board

---
+++ src/chess/pieces/King.java
@@ -0,0 +1,20 @@
+/*@Author Rodrigo Bosio
+ *@since July-2019*/
+package chess.pieces;
+
+import boardgame.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+public class King extends ChessPiece {
+
+	public King(Board board, Color color) {
+		super(board, color);
+	}
+	
+	@Override
+	public String toString() {
+		return "K";
+	}
+
+}
