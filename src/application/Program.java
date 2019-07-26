339371c0300a7001504682a4e79b5d3a030f5a09
Rodrigo Bosio <rodrigo.bosio@fatec.sp.gov.br>
2019-07-26 15:36:20 -0300

    Project Created

---
+++ src/application/Program.java
@@ -0,0 +1,12 @@
+/*@Author Rodrigo Bosio
+ *@since July-2019*/
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello world");
+	}
+
+}
