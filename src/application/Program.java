699f842e1688f69d80072d01c51dcffe4ed289f0
Rodrigo Bosio <rodrigo.bosio@fatec.sp.gov.br>
2019-07-26 15:45:39 -0300

    First Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,11 +2,14 @@
  *@since July-2019*/
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("hello world");
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 	}
 
 }
