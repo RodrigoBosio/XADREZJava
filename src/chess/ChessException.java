3126f25a6c207e43c09b7303d967fb878bb520f7
Rodrigo Bosio <rodrigo.bosio@fatec.sp.gov.br>
2019-07-27 17:05:51 -0300

    ChessException and ChessPosition

---
+++ src/chess/ChessException.java
@@ -0,0 +1,11 @@
+/*@Author Rodrigo Bosio
+ *@since July-2019*/
+package chess;
+
+public class ChessException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ChessException(String msg) {
+		super(msg);
+	}
+}
