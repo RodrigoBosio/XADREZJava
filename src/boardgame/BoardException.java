eb502a3d0529aceba37da8de585f7fb32121092a
Rodrigo Bosio <rodrigo.bosio@fatec.sp.gov.br>
2019-07-27 16:47:17 -0300

    BoardException and defensiva programming

---
+++ src/boardgame/BoardException.java
@@ -0,0 +1,11 @@
+/*@Author Rodrigo Bosio
+ *@since July-2019*/
+package boardgame;
+
+public class BoardException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public BoardException(String msg) {
+		super(msg);
+	}
+}
